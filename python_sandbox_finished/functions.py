9876910a71dbd044cbfcf6b9e96a1e404330658e
romeo adjam <sodangbe@users.noreply.github.com>
2021-05-04 00:16:58 -0500

    finished

--- python_sandbox_finished/functions.py
+++ python_sandbox_finished/functions.py
@@ -1,6 +1,6 @@
 # A function is a block of code which only runs when it is called. In Python, we do not use curly brackets, we use indentation with tabs or spaces
 
-
+import json ,operator
 # Create function
 def sayHello(name='Sam'):
     return (f'Hello {name}')
@@ -20,3 +20,15 @@ getSum = lambda num1, num2: num1 + num2
 print(getSum(10, 3))
 print(sayHello('romeo'))
 print(sayHello())
+
+
+
+def sort_by_price_ascending(json_string):
+    
+    json_parse =json.loads(json_string)
+    sorting_json = json.dumps(json_parse,sort_keys=True)
+    #json_parse.sort(key=operator.itemgetter('price'))
+    #sorted = json.dump(json_parse, indent=2)
+    return  sorting_json
+
+print(sort_by_price_ascending('[{"name":"eggs","price":1},{"name":"coffee","price":9.99},{"name":"rice","price":4.04}]'))
--- python_sandbox_finished/py_json.py
+++ python_sandbox_finished/py_json.py
@@ -16,3 +16,18 @@ car = {'make': 'Ford', 'model': 'Mustang', 'year': 1970}
 carJSON = json.dumps(car)
 
 print(carJSON)
+
+
+
+
+def sort_by_price_ascending(json_string):
+    
+    json_parse =json.loads(json_string)
+    
+    #for it in json_parse
+       #print(y['price'], y[name])
+       sorting_json = json.dumps(json_parse,sort_keys=True)
+
+       return sorting_json
+
+print(sort_by_price_ascending('[{"name":"eggs","price":1},{"name":"coffee","price":9.99},{"name":"rice","price":4.04}]'))
